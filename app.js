5424c0aae63f4cf7029101e4f7253c447bae34d7
sundeep419 <sundeep419@gmail.com>
2017-03-10 00:20:21 +0530

    placeholders

--- app.js
+++ app.js
@@ -29,15 +29,15 @@
 		]
 	},
 	]
-})();
-(function(){
-	app.controller('PanelController',function(){
+	app.controller("PanelController",function(){
 	this.tab=1;
 	this.selectTab=function(setTab){
 		this.tab=setTab;
 	};
 	this.isSelected=function(checkTab){
-		return this.tab=checkTab;
+		return this.tab===checkTab;
 	};
-	})
+	});
 })();
+// (function(){
+// })();
